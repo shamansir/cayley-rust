7b83149e2a7640253e6e336bf79958fb22ffd398
Ulric Wilfred <shaman.sir@gmail.com>
2014-10-19 00:44:14 +0200

    rustdoc

---
+++ implementors/core/clone/trait.Clone.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['cayley'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='cayley/path/struct.Vertex.html' title='cayley::path::Vertex'>Vertex</a>","<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='cayley/path/struct.Morphism.html' title='cayley::path::Morphism'>Morphism</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/serialize/serialize/trait.Decodable.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['cayley'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;S: <a class='trait' href='http://doc.rust-lang.org/nightly/serialize/serialize/trait.Decoder.html' title='serialize::serialize::Decoder'>Decoder</a>&lt;E&gt;, E&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/serialize/serialize/trait.Decodable.html' title='serialize::serialize::Decodable'>Decodable</a>&lt;S, E&gt; for <a class='struct' href='cayley/graph/struct.GraphNode.html' title='cayley::graph::GraphNode'>GraphNode</a>","<a class='stability Unmarked' title='No stability level'></a>impl&lt;S: <a class='trait' href='http://doc.rust-lang.org/nightly/serialize/serialize/trait.Decoder.html' title='serialize::serialize::Decoder'>Decoder</a>&lt;E&gt;, E&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/serialize/serialize/trait.Decodable.html' title='serialize::serialize::Decodable'>Decodable</a>&lt;S, E&gt; for <a class='struct' href='cayley/graph/struct.GraphNodes.html' title='cayley::graph::GraphNodes'>GraphNodes</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
