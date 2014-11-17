6dc0d1530d7a56795935631f494484eed40cb1aa
Ulric Wilfred <shaman.sir@gmail.com>
2014-11-17 23:31:37 +0100

    fixing macro usages in tests

--- tests/30kmoviedata.rs
+++ tests/30kmoviedata.rs
@@ -1,15 +1,17 @@
+#![feature(globs)]
 #![feature(phase, macro_rules)]
 
 #[phase(plugin, link)] extern crate cayley;
 
 use cayley::graph::{Graph, V1};
-use cayley::graph::{GraphNodes, GraphNode};
-use cayley::path::{Morphism, Vertex, Path, Query};
+use cayley::graph::Node as GraphNode;
+use cayley::graph::Nodes as GraphNodes;
+
+use cayley::path::*;
+
 use cayley::selector::{AnyNode, Node};
 use cayley::selector::AnyTag;
 use cayley::selector::Predicate;
-
-use cayley::path::vertex;
 
 #[test]
 fn main() {
--- tests/connection.rs
+++ tests/connection.rs
@@ -1,10 +1,14 @@
-extern crate cayley;
+#![feature(phase, macro_rules)]
+
+#[phase(plugin, link)] extern crate cayley;
 
 use cayley::{Graph, V1};
 
-use cayley::{GraphNode, GraphNodes};
+use cayley::graph::Node;
+use cayley::graph::Nodes;
 
 use cayley::path::{Vertex, Query};
+use cayley::path::All;
 use cayley::selector::AnyNode;
 
 #[test]
@@ -17,13 +21,13 @@ fn main() {
 
     };
 
-    match graph.find(Vertex::start(AnyNode).All()) {
+    match graph.find(vertex!(AnyNode => All)) {
 
         Err(error) => panic!(error.to_string()),
-        Ok(GraphNodes(nodes)) => {
+        Ok(Nodes(nodes)) => {
             assert!(nodes.len() > 0);
             match nodes.iter().next() {
-                Some(&GraphNode(ref first_node)) => {
+                Some(&Node(ref first_node)) => {
                     // node is a HashMap<String, String>
                     println!("{:s}", first_node["id".to_string()]);
                 },
